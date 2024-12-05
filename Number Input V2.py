44d3fd7d5f9cfbcad9ad57cbed9ce22658bbf3b0
DormentCoder <29092866@students.lincoln.ac.uk>
2024-12-05 13:58:30 +0000

    Add files via upload

---
+++ Number Input V2.py
@@ -0,0 +1,8 @@
+#Number Input V2.00 by Dc117 Corp.
+
+print('Number Input V2.00')
+choice = input('Enter a number 1-3: ')
+if choice > "0" and choice < "3":
+    print('Valid input')
+else:
+    print('Invalid input')
